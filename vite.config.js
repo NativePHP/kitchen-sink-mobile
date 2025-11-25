0dcf4022303a46bf0bc82ae3993f2c7300ad55ca
Simon Hamp <simon.hamp@me.com>
2025-11-25 19:48:13 +0000

    HMR, Native command

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
-import {nativephpMobile} from './vendor/nativephp/mobile/resources/inertia/nativephp-vite.js';
+import { nativephpMobile } from './vendor/nativephp/mobile/resources/js/vite-plugin.js';
 
-
-
-export default defineConfig(() => {
-    const mobileConfig = nativephpMobile();
-
-    return {
-        ...nativephpMobile(),
-        plugins: [
-            ...(mobileConfig.plugins || []),
-            laravel({
-                input: ['resources/css/app.css', 'resources/js/app.js'],
-                refresh: true,
-            }),
-            tailwindcss(),
-        ],
-    };
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+        nativephpMobile(),
+    ]
 });
