ee87fd5a64334bc1bb087ceb701084f13542ded8
Shane Rosenthal <srosenthal82@gmail.com>
2025-11-26 22:04:45 -0500

    Fixed

--- app/Livewire/Camera/Video.php
+++ app/Livewire/Camera/Video.php
@@ -68,14 +68,6 @@ class Video extends Component
     #[On('media-share')]
     public function shareVideo(string $path): void
     {
-        dd([
-            'filesystems.default' => config('filesystems.default'),
-            'FILESYSTEM_DISK_env' => env('FILESYSTEM_DISK'),
-            'getenv_FILESYSTEM_DISK' => getenv('FILESYSTEM_DISK'),
-            'public_disk_url' => config('filesystems.disks.public.url'),
-            'Storage_default_driver' => Storage::getDefaultDriver(),
-        ]);
-
         Share::file('Check this out!', 'Check this out!', Storage::path($path));
     }
 
