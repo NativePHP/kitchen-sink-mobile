b5a2cd38adeeb6f69f0da8d6b466aeebe772de74
Simon Hamp <simon.hamp@me.com>
2025-11-27 15:49:25 +0000

    Reorg

--- resources/views/livewire/native-edge.blade.php
+++ resources/views/livewire/native-edge.blade.php
@@ -34,16 +34,16 @@
             <native:side-nav-item active="{{ request()->routeIs('dialog.alert') }}" id="dialog-alert" icon="bell" url="{{ route('dialog.alert') }}" label="Alert"/>
             <native:side-nav-item active="{{ request()->routeIs('dialog.toast') }}" id="dialog-toast" icon="bolt" url="{{ route('dialog.toast') }}" label="Toast"/>
         </native:side-nav-group>
-        <native:side-nav-item active="{{ request()->routeIs('network') }}" id="network-demo" icon="globe" url="{{ route('network') }}" label="Network" badge="New!" badge-color="blue"/>
-        <native:side-nav-item active="{{ request()->routeIs('scanner') }}" id="scanner-demo" icon="qrcode" url="{{ route('scanner') }}" label="Scanner Demo" badge="New!" badge-color="blue"/>
-        <native:side-nav-item active="{{ request()->routeIs('microphone') }}" id="microphone-demo" icon="microphone" url="{{ route('microphone') }}" label="Microphone" badge="New!" badge-color="blue"/>
-        <native:side-nav-item active="{{ request()->routeIs('device') }}" id="device-demo" icon="device-phone-mobile" url="{{ route('device') }}" label="Device Info"/>
-        <native:side-nav-item active="{{ request()->routeIs('haptics.vibrate') }}" id="haptics-vibrate" icon="vibrate" url="{{ route('haptics.vibrate') }}" label="Haptics"/>
-        <native:side-nav-item active="{{ request()->routeIs('browser') }}" id="browser-demo" icon="globe-alt" url="{{ route('browser') }}" label="Browser"/>
-        <native:side-nav-item active="{{ request()->routeIs('flashlight') }}" id="system-flashlight" icon="lightbulb" url="{{ route('flashlight') }}" label="Flashlight"/>
-        <native:side-nav-item active="{{ request()->routeIs('push-notifications') }}" id="push-notifications-demo" icon="bell" url="{{ route('push-notifications') }}" label="Push Notifications"/>
         <native:side-nav-item active="{{ request()->routeIs('biometrics') }}" id="biometrics-demo" icon="finger-print" url="{{ route('biometrics') }}" label="Biometrics"/>
+        <native:side-nav-item active="{{ request()->routeIs('browser') }}" id="browser-demo" icon="globe-alt" url="{{ route('browser') }}" label="Browser"/>
+        <native:side-nav-item active="{{ request()->routeIs('device') }}" id="device-demo" icon="device-phone-mobile" url="{{ route('device') }}" label="Device Info"/>
+        <native:side-nav-item active="{{ request()->routeIs('flashlight') }}" id="system-flashlight" icon="lightbulb" url="{{ route('flashlight') }}" label="Flashlight"/>
         <native:side-nav-item active="{{ request()->routeIs('geolocation.getCurrent') }}" id="geolocation-get-current" icon="map" url="{{ route('geolocation.getCurrent') }}" label="Geolocation"/>
+        <native:side-nav-item active="{{ request()->routeIs('haptics.vibrate') }}" id="haptics-vibrate" icon="vibrate" url="{{ route('haptics.vibrate') }}" label="Haptics"/>
+        <native:side-nav-item active="{{ request()->routeIs('microphone') }}" id="microphone-demo" icon="microphone" url="{{ route('microphone') }}" label="Microphone" badge="New!" badge-color="blue"/>
+        <native:side-nav-item active="{{ request()->routeIs('network') }}" id="network-demo" icon="globe" url="{{ route('network') }}" label="Network" badge="New!" badge-color="blue"/>
+        <native:side-nav-item active="{{ request()->routeIs('push-notifications') }}" id="push-notifications-demo" icon="bell" url="{{ route('push-notifications') }}" label="Push Notifications"/>
+        <native:side-nav-item active="{{ request()->routeIs('scanner') }}" id="scanner-demo" icon="qrcode" url="{{ route('scanner') }}" label="Scanner" badge="New!" badge-color="blue"/>
         <native:side-nav-item active="{{ request()->routeIs('secure-storage') }}" id="secure-storage-demo" icon="folder-lock" url="{{ route('secure-storage') }}" label="Secure Storage"/>
         <native:horizontal-divider />
         <native:side-nav-group heading="Resources" :expanded="false">
