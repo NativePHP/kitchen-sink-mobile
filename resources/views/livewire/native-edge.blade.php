fae317046648189543ad35d62c495dc591b3c590
Shane Rosenthal <srosenthal82@gmail.com>
2025-12-12 17:28:22 -0500

    Updates mobile to test ios bf builds

--- resources/views/livewire/native-edge.blade.php
+++ resources/views/livewire/native-edge.blade.php
@@ -12,7 +12,7 @@
 
         <native:top-bar-action
             id="docs"
-            icon="book-open"
+            icon="{{\Native\Mobile\Facades\System::isIos() ? 'book-open' : 'menu_book'}}"
             label="Docs"
             url="https://nativephp.com/docs/mobile/2/getting-started/introduction"
         />
@@ -28,22 +28,22 @@
         <native:side-nav-group heading="Camera" :expanded="request()->routeIs('camera.*')">
             <native:side-nav-item active="{{ request()->routeIs('camera.camera') }}" id="camera-get-photo" icon="camera" url="{{ route('camera.camera') }}" label="Camera"/>
             <native:side-nav-item active="{{ request()->routeIs('camera.gallery') }}" id="camera-pick-images" icon="image-plus" url="{{ route('camera.gallery') }}" label="Gallery"/>
-            <native:side-nav-item active="{{ request()->routeIs('camera.video') }}" id="camera-video" icon="video" url="{{ route('camera.video') }}" label="Video Recorder" badge="New!" badge-color="blue"/>
+            <native:side-nav-item active="{{ request()->routeIs('camera.video') }}" id="camera-video" icon="{{\Native\Mobile\Facades\System::isIos() ? 'video' : 'videocam'}}" url="{{ route('camera.video') }}" label="Video Recorder" badge="New!" badge-color="blue"/>
         </native:side-nav-group>
         <native:side-nav-group heading="Dialog" :expanded="request()->routeIs('dialog.*')">
-            <native:side-nav-item active="{{ request()->routeIs('dialog.alert') }}" id="dialog-alert" icon="bell" url="{{ route('dialog.alert') }}" label="Alert"/>
-            <native:side-nav-item active="{{ request()->routeIs('dialog.toast') }}" id="dialog-toast" icon="bolt" url="{{ route('dialog.toast') }}" label="Toast"/>
+            <native:side-nav-item active="{{ request()->routeIs('dialog.alert') }}" id="dialog-alert" icon="{{\Native\Mobile\Facades\System::isIos() ? 'bell' : 'message'}}" url="{{ route('dialog.alert') }}" label="Alert"/>
+            <native:side-nav-item active="{{ request()->routeIs('dialog.toast') }}" id="dialog-toast" icon="{{\Native\Mobile\Facades\System::isIos() ? 'bolt' : 'flag'}}" url="{{ route('dialog.toast') }}" label="Toast"/>
         </native:side-nav-group>
-        <native:side-nav-item active="{{ request()->routeIs('biometrics') }}" id="biometrics-demo" icon="finger-print" url="{{ route('biometrics') }}" label="Biometrics"/>
-        <native:side-nav-item active="{{ request()->routeIs('browser') }}" id="browser-demo" icon="globe-alt" url="{{ route('browser') }}" label="Browser"/>
-        <native:side-nav-item active="{{ request()->routeIs('device') }}" id="device-demo" icon="device-phone-mobile" url="{{ route('device') }}" label="Device Info"/>
-        <native:side-nav-item active="{{ request()->routeIs('flashlight') }}" id="system-flashlight" icon="lightbulb" url="{{ route('flashlight') }}" label="Flashlight"/>
+        <native:side-nav-item active="{{ request()->routeIs('biometrics') }}" id="biometrics-demo" icon="{{\Native\Mobile\Facades\System::isIos() ? 'die.face.5' : 'fingerprint'}}" url="{{ route('biometrics') }}" label="Biometrics"/>
+        <native:side-nav-item active="{{ request()->routeIs('browser') }}" id="browser-demo" icon="{{\Native\Mobile\Facades\System::isIos() ? 'person.line.dotted.person' : 'travel_explore'}}" url="{{ route('browser') }}" label="Browser"/>
+        <native:side-nav-item active="{{ request()->routeIs('device') }}" id="device-demo" icon="{{\Native\Mobile\Facades\System::isIos() ? 'shoeprints.fill' : 'app_shortcut'}}" url="{{ route('device') }}" label="Device Info"/>
+        <native:side-nav-item active="{{ request()->routeIs('flashlight') }}" id="system-flashlight" icon="{{\Native\Mobile\Facades\System::isIos() ? 'flashlight.on.fill' : 'flashlight_on'}}" url="{{ route('flashlight') }}" label="Flashlight"/>
         <native:side-nav-item active="{{ request()->routeIs('geolocation.getCurrent') }}" id="geolocation-get-current" icon="map" url="{{ route('geolocation.getCurrent') }}" label="Geolocation"/>
-        <native:side-nav-item active="{{ request()->routeIs('haptics.vibrate') }}" id="haptics-vibrate" icon="vibrate" url="{{ route('haptics.vibrate') }}" label="Haptics"/>
-        <native:side-nav-item active="{{ request()->routeIs('microphone') }}" id="microphone-demo" icon="microphone" url="{{ route('microphone') }}" label="Microphone" badge="New!" badge-color="blue"/>
-        <native:side-nav-item active="{{ request()->routeIs('network') }}" id="network-demo" icon="globe" url="{{ route('network') }}" label="Network" badge="New!" badge-color="blue"/>
-        <native:side-nav-item active="{{ request()->routeIs('push-notifications') }}" id="push-notifications-demo" icon="bell" url="{{ route('push-notifications') }}" label="Push Notifications"/>
-        <native:side-nav-item active="{{ request()->routeIs('scanner') }}" id="scanner-demo" icon="qrcode" url="{{ route('scanner') }}" label="Scanner" badge="New!" badge-color="blue"/>
+        <native:side-nav-item active="{{ request()->routeIs('haptics.vibrate') }}" id="haptics-vibrate" icon="{{\Native\Mobile\Facades\System::isIos() ? 'vibrate' : 'vibration'}}" url="{{ route('haptics.vibrate') }}" label="Haptics"/>
+        <native:side-nav-item active="{{ request()->routeIs('microphone') }}" id="microphone-demo" icon="{{\Native\Mobile\Facades\System::isIos() ? 'microphone.circle' : 'app_shortcut'}}" url="{{ route('microphone') }}" label="Microphone" badge="New!" badge-color="blue"/>
+        <native:side-nav-item active="{{ request()->routeIs('network') }}" id="network-demo" icon="{{\Native\Mobile\Facades\System::isIos() ? 'globe' : 'network_check'}}" url="{{ route('network') }}" label="Network" badge="New!" badge-color="blue"/>
+        <native:side-nav-item active="{{ request()->routeIs('push-notifications') }}" id="push-notifications-demo" icon="{{\Native\Mobile\Facades\System::isIos() ? 'bell' : 'circle_notifications'}}" url="{{ route('push-notifications') }}" label="Push Notifications"/>
+        <native:side-nav-item active="{{ request()->routeIs('scanner') }}" id="scanner-demo" icon="{{\Native\Mobile\Facades\System::isIos() ? 'qrcode' : 'qr_code_scanner'}}" url="{{ route('scanner') }}" label="Scanner" badge="New!" badge-color="blue"/>
         <native:side-nav-item active="{{ request()->routeIs('secure-storage') }}" id="secure-storage-demo" icon="lock" url="{{ route('secure-storage') }}" label="Secure Storage"/>
         <native:horizontal-divider />
         <native:side-nav-group heading="Resources" :expanded="false">
@@ -56,7 +56,7 @@
             id="scanner"
             label="Scanner"
             url="{{ route('scanner') }}"
-            icon="qrcode"
+            icon="{{\Native\Mobile\Facades\System::isIos() ? 'qrcode' : 'qr_code_scanner'}}"
             :active="request()->routeIs('scanner')"
             news="true"
         />
@@ -64,21 +64,21 @@
             id="camera"
             label="Video"
             url="{{ route('camera.video') }}"
-            icon="video"
+            icon="{{\Native\Mobile\Facades\System::isIos() ? 'video' : 'videocam'}}"
             :active="request()->routeIs('camera.video')"
         />
         <native:bottom-nav-item
             id="microphone"
             label="Microphone"
             url="{{ route('microphone') }}"
-            icon="microphone"
+            icon="{{\Native\Mobile\Facades\System::isIos() ? 'microphone' : 'record_voice_over'}}"
             :active="request()->routeIs('microphone')"
         />
         <native:bottom-nav-item
             id="network"
             label="Network"
             url="{{ route('network') }}"
-            icon="globe"
+            icon="{{\Native\Mobile\Facades\System::isIos() ? 'globe' : 'network_check'}}"
             :active="request()->routeIs('network')"
         />
     </native:bottom-nav>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
-import {nativephpMobile} from './vendor/nativephp/mobile/resources/js/vite-plugin';
+import {nativephpMobile, nativephpHotFile} from './vendor/nativephp/mobile/resources/js/vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            hotFile: nativephpHotFile()
         }),
         tailwindcss(),
         nativephpMobile()
